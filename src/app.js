46f9101953074ec25f7c6d6a577539059f08e58e
Nikola Nenovski <nicknenovski@gmail.com>
2024-11-29 20:00:01 +0200

    feat(cors): add cors functionality

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from 'cors';
 import apiRouter from "./routes/apiRouter.js";
 
 const app = express();
 const port = 3000;
 
 app.use("/api", apiRouter);
+
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);

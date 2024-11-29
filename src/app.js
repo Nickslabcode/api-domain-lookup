6520088f0c1ee8bf23323980394b4b32009f1c40
Nikola Nenovski <nicknenovski@gmail.com>
2024-11-29 19:36:30 +0200

    refactor(app.js): refactor app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import apiRouter from './routes/apiRouter.js'
+import express from "express";
+import apiRouter from "./routes/apiRouter.js";
 
 const app = express();
 const port = 3000;
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
-})
+});

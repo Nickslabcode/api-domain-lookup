3f85cce8da310d7b0ae9e109fe69c14253241b1e
Nikola Nenovski <nicknenovski@gmail.com>
2024-11-29 16:37:37 +0200

    chore(v1routes): merge conflicts from cdn-router branch

--- src/routes/v1Routes.js
+++ src/routes/v1Routes.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import cdnCheckHandler from '../handlers/cdnCheck.handler.js';
+import wpCheckHandler from '../handlers/wpCheck.handler.js';
 
 const router = express.Router();
 
-router.get('/wp-check', (req, res) => {
-  res.send(req.path);
-})
+router.get('/wp-check', wpCheckHandler)
 
 router.get('/cdn-check', cdnCheckHandler);
 

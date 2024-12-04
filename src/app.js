b28b1e08c9e76168e36e0ee555397545fa52f78e
Nikola Nenovski <nicknenovski@gmail.com>
2024-12-04 17:24:07 +0200

    refactor(app): refactor code to work as AWS Lambda function
    BREAKING CHANGE: Does not work as a standalone express server app anymore, instead it's designed to
    work as a serverless app

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import express from "express";
-import cors from 'cors';
-import apiRouter from "./routes/apiRouter.js";
+import cors from "cors";
+import wpCheckHandler from "./handlers/wpCheck.handler.js";
+import cdnCheckHandler from "./handlers/cdnCheck.handler.js";
 
 const app = express();
-const port = 3000;
-
-app.use("/api", apiRouter);
-
+app.use(express.json());
 app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+app.get("/api/v1/wp-check", wpCheckHandler);
+
+app.get("/api/v1/cdn-check", cdnCheckHandler);
+
+export default app;

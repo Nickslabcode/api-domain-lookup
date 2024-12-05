fb46dccba63749a7b5f2a3633ff0279b87802630
Nikola Nenovski <nicknenovski@gmail.com>
2024-12-05 19:21:21 +0200

    feat(cdn-check): implement cdn-check handler to check the response headers for various CDN headers

--- src/handlers/cdnCheck.handler.js
+++ src/handlers/cdnCheck.handler.js
@@ -2,14 +2,47 @@ const cdnCheckHandler = async (req, res) => {
   const domain = req.query.domain;
 
   if (!domain) {
-    return res.status(400).send('Domain query parameter is required!');
+    return res.status(400).send("Domain query parameter is required!");
   }
 
-  const response = await fetch(`https://${domain}`);
-  const headers = response.headers;
+  try {
+    const response = await fetch(`https://${domain}`);
 
-  // SiteGround CDN check
-  res.send(headers);
+    const getHeader = (headerName) => response.headers.get(headerName);
+    const cdnHeadersObject = {};
+
+    switch (true) {
+      case !!getHeader("x-sg-cdn"):
+        cdnHeadersObject.siteGround = true;
+
+      case !!getHeader("cf-cache-status"):
+        cdnHeadersObject.cloudFlare = true;
+
+      case !!getHeader("x-bunny-cache"):
+        cdnHeadersObject.bunny = true;
+
+      case getHeader("x-cache")?.includes("HIT from cloudFront"):
+        cdnHeadersObject.cloudFront = true;
+
+      case !!getHeader("x-akamai-transformed"):
+        cdnHeadersObject.akamai = true;
+
+      case !!getHeader("x-sp-cache-status"):
+        cdnHeadersObject.stackPath = true;
+
+      case !!getHeader("x-azure-ref"):
+        cdnHeadersObject.azure = true;
+
+      default:
+        break;
+    }
+
+    res.status(200).json(cdnHeadersObject);
+  } catch (error) {
+    res
+      .status(500)
+      .send("Internal server error. Please try again later", error);
+  }
 
   // TODO add more CDN header checks: https://chatgpt.com/c/6748a119-9d58-800c-aab9-75d4ae1d1667
 };

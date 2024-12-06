fbe187ab496ef2a4f2b689274c8fd1a5d75a6bf8
Nikola Nenovski <nicknenovski@gmail.com>
2024-12-06 15:49:14 +0200

    feat(requestheaders): add request headers helper
    Integrated a fn to return the request headers with a dynamic Referrer URL based on the domain that
    the app is checking

---
+++ src/common/requestHeaders.js
@@ -0,0 +1,9 @@
+export const requestHeaders = domain => ({
+  "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36",
+  Accept:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+  "Accept-Language": "en-US,en;q=0.9",
+  Referer: `https://domainlookup.nikola-nenovski.info/results?domain=${domain}`,
+  Connection: "keep-alive",
+});
--- src/handlers/cdnCheck.handler.js
+++ src/handlers/cdnCheck.handler.js
@@ -1,3 +1,5 @@
+import { requestHeaders } from "../common/requestHeaders";
+
 const cdnCheckHandler = async (req, res) => {
   const domain = req.query.domain;
 
@@ -5,32 +7,37 @@ const cdnCheckHandler = async (req, res) => {
     return res.status(400).send("Domain query parameter is required!");
   }
 
-  try {
-    const response = await fetch(`https://${domain}`);
+  const headers = requestHeaders(domain);
 
-    const getHeader = (headerName) => response.headers.get(headerName);
+  try {
+    const response = await fetch(`https://${domain}`, {
+      headers,
+    });
+
+    const getResponseHeader = (headerName) => response.headers.get(headerName);
+
     const cdnHeadersObject = {};
 
     switch (true) {
-      case !!getHeader("x-sg-cdn"):
+      case !!getResponseHeader("x-sg-cdn"):
         cdnHeadersObject.siteGround = true;
 
-      case !!getHeader("cf-cache-status"):
+      case !!getResponseHeader("cf-cache-status"):
         cdnHeadersObject.cloudFlare = true;
 
-      case !!getHeader("x-bunny-cache"):
+      case !!getResponseHeader("x-bunny-cache"):
         cdnHeadersObject.bunny = true;
 
-      case getHeader("x-cache")?.includes("HIT from cloudFront"):
+      case getResponseHeader("x-cache")?.includes("HIT from cloudFront"):
         cdnHeadersObject.cloudFront = true;
 
-      case !!getHeader("x-akamai-transformed"):
+      case !!getResponseHeader("x-akamai-transformed"):
         cdnHeadersObject.akamai = true;
 
-      case !!getHeader("x-sp-cache-status"):
+      case !!getResponseHeader("x-sp-cache-status"):
         cdnHeadersObject.stackPath = true;
 
-      case !!getHeader("x-azure-ref"):
+      case !!getResponseHeader("x-azure-ref"):
         cdnHeadersObject.azure = true;
 
       default:
@@ -43,8 +50,6 @@ const cdnCheckHandler = async (req, res) => {
       .status(500)
       .send("Internal server error. Please try again later", error);
   }
-
-  // TODO add more CDN header checks: https://chatgpt.com/c/6748a119-9d58-800c-aab9-75d4ae1d1667
 };
 
 export default cdnCheckHandler;

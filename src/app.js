c570716f5c916c1640a05b0d3ca983574b1002c9
Nickslabcode <nicknenovski@gmail.com>
2024-12-24 17:11:08 +0200

    Update cors policy

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
-import express from "express";
-import cors from "cors";
-import wpCheckHandler from "./handlers/wpCheck.handler.js";
-import cdnCheckHandler from "./handlers/cdnCheck.handler.js";
+import express from 'express';
+import cors from 'cors';
+import wpCheckHandler from './handlers/wpCheck.handler.js';
+import cdnCheckHandler from './handlers/cdnCheck.handler.js';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://domain-lookup.nikola-nenovski.info',
+    ],
+    methods: ['GET'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
-app.get("/api/v1/wp-check", wpCheckHandler);
+app.get('/api/v1/wp-check', wpCheckHandler);
 
-app.get("/api/v1/cdn-check", cdnCheckHandler);
+app.get('/api/v1/cdn-check', cdnCheckHandler);
 
 export default app;
